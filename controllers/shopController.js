e415f5521adfad66ab07660dea9fcb710350d4e2
samitkapoor <samitkapoor77@gmail.com>
2023-04-26 13:05:56 +0530

    Fetching products from mongodb

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,23 +1,24 @@
+const getDb = require("../util/database").getDb;
+
 const Product = require("../models/product");
 
 var products = [];
 
 // fetch all the products when the page loads
-fetchProducts = () => {
-  products = [
-    new Product(
-      "Nike",
-      "Air Max",
-      "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSql8QKYsLlb3b_avlRm9BBM1o_Wkq0p6O_601QyOH38stNpUumeqQyDD8x7hUKc8STYKcQ9vS0yTwG3ENx_qkEnVZd2jEXkHaDn-XjbPvmQSekMWIZ6dsPOg",
-      "16,995.00"
-    ),
-    new Product(
-      "Sony",
-      "PS5 PlayStation Console",
-      "https://gmedia.playstation.com/is/image/SIEPDC/ps5-product-thumbnail-01-en-14sep21?$facebook$",
-      "49,990.00"
-    ),
-  ];
+fetchProducts = async () => {
+  let db = getDb();
+
+  await db
+    .collection("products")
+    .find()
+    .toArray()
+    .then((product) => {
+      products = product;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
 module.exports.getHomePage = (req, res, next) => {
--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback = () => {}) => {
   MongoClient.connect(
-    "mongodb+srv://samitkapoor77:XqbU6IZ2klCEkB15@cluster0.qj4hqtz.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://samitkapoor77:XqbU6IZ2klCEkB15@cluster0.qj4hqtz.mongodb.net/upstore?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
